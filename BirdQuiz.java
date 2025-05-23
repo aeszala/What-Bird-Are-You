6b6802b54024ae70160fd1984d1d917d020b4b7c
aeszala <aeszala@gmail.com>
2025-05-23 01:58:51 -0400

    created BirdQuiz, BirdLoader, and QuestionLoader classes

---
+++ BirdQuiz.java
@@ -0,0 +1,36 @@
+﻿import java.util.ArrayList;
+import java.util.List;
+
+public class BirdQuiz {
+    private List<Bird> birds;
+    private List<Question> questions;
+
+    public BirdQuiz(ArrayList<Bird> birds, ArrayList<Question> questions) {
+        this.birds = birds;
+        this.questions = questions;
+    }
+
+    public List<Bird> getBirds() {
+        return new ArrayList<Bird>(this.birds);
+    }
+
+    public List<Question> getQuestions() {
+        return new ArrayList<Question>(this.questions);
+    }
+
+    public boolean setBirds(List<Bird> birds) {
+        if (birds != null) {
+            this.birds = birds;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean setQuestions(List<Question> questions) {
+        if (questions != null) {
+            this.questions = questions;
+            return true;
+        }
+        return false;
+    }
+}
